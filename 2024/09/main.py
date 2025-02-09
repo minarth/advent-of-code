a0d2e2888df70963582933f640859788fde55fea
Martin Münch <minarth@gmail.com>
2025-02-09 10:04:03 +0100

    09 2024 cleanup

--- 2024/09/main.py
+++ 2024/09/main.py
@@ -39,87 +39,6 @@ def part_one(files, empty):
             idx += 1
     return counter
     
-@dataclass
-class DL:
-    "double linked list - minimalistic"
-    prev: Any
-    nxt:  Any 
-    idx: int
-    value: int
-    usable: bool = True
-
-    def __repr__(self):
-        return f"{self.idx} {self.value} slf:{id(self)} prv:{id(self.prev)} nxt:{id(self.nxt)} N:{id(None)}"
-
-def print_list(start: DL):
-    # prepare for print
-    node = start
-    string = ""
-    while node is not None: 
-        for _ in range(node.value):
-            string += str(node.idx) if node.idx >= 0 else "."
-        node = node.nxt
-    print(string)
-
-
-def part_two_failed(files, empty):
-    # the linked list was not my best idea for implementation, debug is nightmare
-    end = None
-    files_list = []
-    for i, (f, e) in enumerate(zip(files, empty)):
-        val_node = DL(prev=end,nxt=None, idx=i, value=f)
-        files_list.append(val_node)
-        empty_node = DL(prev=val_node, nxt=None, idx=-1, value=e)
-        val_node.nxt = empty_node
-        if end is not None:
-            end.nxt = val_node
-        end = empty_node
-    
-    if len(files) > len(empty):
-        val_node = DL(prev=end, nxt=None, idx=i+1, value=files[-1])
-        files_list.append(val_node)
-        end.nxt = val_node
-        end = val_node
-
-    start = end
-    while start.prev is not None: start = start.prev    
-    
-    # single walkthrough to fill the spaces
-    for node in files_list[::-1]:
-        while start.prev is not None: start = start.prev        
-        search_n = start
-        print(f"Moving? {node}")
-        print_list(start)
-
-        while search_n is not None and search_n.idx != node.idx:
-            if search_n.idx == -1 and search_n.value >= node.value:
-                print(f"Found a match {node} \nw {search_n}")
-                search_n.value -= node.value
-                empty = DL(prev=node.prev, nxt=node.nxt, idx=-1, value=node.value)
-                if node.prev is not None: node.prev.nxt = empty
-                if node.nxt  is not None: node.nxt.prev = empty
-
-                node.prev = search_n.prev
-                node.nxt = search_n
-                if search_n.prev is not None: search_n.prev.nxt = node
-                if search_n.nxt  is not None: search_n.nxt.prev = node
-                search_n.prev = node
-                break
-            search_n = search_n.nxt
-        node.usable = False
-
-    # calc ids
-    while start.prev is not None: start = start.prev
-    counter = 0
-    position = 0
-    while node is not None:
-        for _ in range(node.value):
-            if node.idx >= 0: counter += node.idx*position
-            position += 1
-
-        node = node.nxt
-    return counter
-
 
 def print_mem(memory):
     mem_s = ""
@@ -163,7 +82,6 @@ def part_two(files, empties):
 
 if __name__ == "__main__":
     f,e = process("input")
-    #print(part_one(f,e))
+    print(part_one(f,e))
     print(part_two(f,e))
-    # too low 6361299053820
     
